3c3ea3b4824f0854bbed2296a2c2888c4c3a103d
Gaurav Kumar <sensiblemindz@gmail.com>
2024-07-30 22:10:10 +0530

    xsd to java file

---
+++ src/test/java/com/xsdtojava/SpringBootXsdtojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsdtojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXsdtojavaApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
